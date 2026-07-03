255ac09632dc7068f60772a748deb5f3dcf91a16
Nathanielhitz <nathanielhitzerd1999@gmail.com>
2026-07-03 20:00:08 +0200

    fix: scheef kruisje in mobiel menu (conflicterende top-klassen vervangen door transforms)

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -30,9 +30,9 @@ export default function MobileNav() {
       >
         {/* hamburger / kruis, puur CSS zodat er geen icon-dependency nodig is */}
         <span className="relative block h-4 w-5" aria-hidden>
-          <span className={`absolute left-0 top-0 h-0.5 w-5 bg-current transition ${open ? "top-2 rotate-45" : ""}`} />
-          <span className={`absolute left-0 top-2 h-0.5 w-5 bg-current transition ${open ? "opacity-0" : ""}`} />
-          <span className={`absolute left-0 top-4 h-0.5 w-5 bg-current transition ${open ? "top-2 -rotate-45" : ""}`} />
+          <span className={`absolute left-0 top-0 h-0.5 w-5 bg-current transition-transform ${open ? "translate-y-2 rotate-45" : ""}`} />
+          <span className={`absolute left-0 top-2 h-0.5 w-5 bg-current transition-opacity ${open ? "opacity-0" : ""}`} />
+          <span className={`absolute left-0 top-4 h-0.5 w-5 bg-current transition-transform ${open ? "-translate-y-2 -rotate-45" : ""}`} />
         </span>
       </button>
 
